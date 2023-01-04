ec33e2e70804d182252c6061d7a0d85861c15c3b
eddy <imanaidielham@gmail.com>
2023-01-04 14:31:58 +0800

    data for profile

---
+++ src/data/faculty.ts
@@ -0,0 +1,46 @@
+export const allFaculties = [
+    {
+        id: 1,
+        faculty: "Faculty of Built Environment",
+    },
+    {
+        id: 2,
+        faculty: "Faculty of Civil Engineering",
+    },
+    {
+        id: 3,
+        faculty: "Faculty of Electrical Engineering",
+    },
+    {
+        id: 4,
+        faculty: "Faculty of Chemical & Energy Engineering",
+    },
+    {
+        id: 5,
+        faculty: "Faculty of Mechanical Engineering",
+    },
+    {
+        id: 6,
+        faculty: "Faculty of Geoinformation & Real Estate",
+    },
+    {
+        id: 7,
+        faculty: "Faculty of Management",
+    },
+    {
+        id: 8,
+        faculty: "Faculty of Science",
+    },
+    {
+        id: 9,
+        faculty: "Faculty of Computing",
+    },
+    {
+        id: 10,
+        faculty: "Faculty of Bioscience & Medical Engineering",
+    }, 
+    {
+        id: 11,
+        faculty: "Malaysia- Japan International Institute of Technology",
+    }, 
+  ];
